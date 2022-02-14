181bbcddee6ccde45d0e8e081f54c5e2133a2d6a
alemica97 <alessandro.micalizzi08@gmail.com>
2022-02-14 18:05:05 +0100

    check iniziale tramite un controllo con if else

--- Pari e Dispari/js/app.js
+++ Pari e Dispari/js/app.js
@@ -3,8 +3,8 @@ function randomMinMax(min, max){
     return Math.floor(Math.random() * max + min); //così ottengo un numero random che va da min a max
 }
 
-let pcNumber = randomMinMax(1, 6);
-console.log(pcNumber);
+// let pcNumber = randomMinMax(1, 6);
+// console.log(pcNumber);
 
 function pariDispari(num1, num2){
     let sum = num1 + num2;
@@ -16,13 +16,28 @@ function pariDispari(num1, num2){
     }
 }
 
-let a = 4;
-let b = 6;
-console.log(a,b);
+// let a = 4;
+// let b = 6;
+// console.log(a,b);
 
-let pD = pariDispari(a, b);
-if(pD == true){
-    console.log('La somma è pari');
+// let pD = pariDispari(a, b);
+// if(pD == true){
+//     console.log('La somma è pari');
+// }else{
+//     console.log('è dispari');
+// }
+let userNumber;
+
+let scelta = prompt('Scegli, pari o dispari? Scrivendo o la parola \'pari\' o \'dispari\'');
+console.log(scelta);
+
+if(scelta == 'pari' || scelta == 'dispari'){
+    userNumber = parseFloat(prompt('Inserisci un numero:'));
+    console.log('Il numero inserito dall\'utente è:', userNumber);
 }else{
-    console.log('è dispari');
+    alert('Errore!');
 }
+
+
+
+

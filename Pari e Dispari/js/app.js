d6a05e8f3d89437d96b31a18f552257c11a11a63
alemica97 <alessandro.micalizzi08@gmail.com>
2022-02-14 18:34:42 +0100

    Ho completato l'esercizio invocando le funzioni precedentemente create, ed ho eseguito dei controlli usando if ed else.

--- Pari e Dispari/js/app.js
+++ Pari e Dispari/js/app.js
@@ -2,10 +2,7 @@
 function randomMinMax(min, max){
     return Math.floor(Math.random() * max + min); //così ottengo un numero random che va da min a max
 }
-
-// let pcNumber = randomMinMax(1, 6);
-// console.log(pcNumber);
-
+//Creo una funzione che dati due numeri, li somma e ritorna true se la somma è pari e false se è dispari
 function pariDispari(num1, num2){
     let sum = num1 + num2;
 
@@ -16,24 +13,40 @@ function pariDispari(num1, num2){
     }
 }
 
-// let a = 4;
-// let b = 6;
-// console.log(a,b);
-
-// let pD = pariDispari(a, b);
-// if(pD == true){
-//     console.log('La somma è pari');
-// }else{
-//     console.log('è dispari');
-// }
-let userNumber;
-
 let scelta = prompt('Scegli, pari o dispari? Scrivendo o la parola \'pari\' o \'dispari\'');
-console.log(scelta);
-
-if(scelta == 'pari' || scelta == 'dispari'){
-    userNumber = parseFloat(prompt('Inserisci un numero:'));
+console.log('Hai scelto', scelta);
+//controllo che l'utente abbia scritto correttamente
+if( scelta == 'pari' || scelta == 'dispari' ){
+    //chiedo all'utente un numero tra 1 e 5
+    let userNumber = parseFloat(prompt('Inserisci un numero compreso tra 1 e 5:'));
     console.log('Il numero inserito dall\'utente è:', userNumber);
+    //faccio il controllo per essere sicuro che il numero sia valido
+    if( isNaN(userNumber) || userNumber > 5 ){
+        alert('Errore, il numero deve essere compreso tra 1 e 5!!');
+    }else{
+        let risultato;
+        //invoco la funzione per assegnare un numero al pc
+        let pcNumber = randomMinMax(1, 5);
+        console.log('Il numero random del PC è:', pcNumber);
+        //invoco la funzione per saper se è pari o dispari
+        let pD = pariDispari(userNumber, pcNumber);
+        console.log('true = pari, false = dispari', pD);
+
+        if(pD == true){
+            risultato = 'pari';
+        }else{
+            risultato = 'dispari';
+        }
+        //controllo il vincitore controllando che la stringa inserita dall'utente sia uguale al risultato
+        if(scelta == risultato){
+            console.log('Complimenti, hai vinto!!');
+        }else{
+            console.log('Mi dispiace, ha vinto il PC');
+        }
+    }
+
+    
+
 }else{
     alert('Errore!');
 }
